1dfb25182d90c9620e0a2bc3ba7f51c9abf9060f
Sergiy Parkhuts <serparkhuts17@gmail.com>
2025-02-13 17:32:50 +0200

    Add tests for Url model

--- spec/factories/url.rb
+++ spec/factories/url.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :url do
-    original_url { 'https://google.com' }
+    # the reason we use SecureRandom.base58(10) here but not GenerateString.call
+    # is because GenerateString.call could be mocked
+
+    original_url { "https://google.com/#{SecureRandom.base58(10)}" }
     slug { GenerateString.call }
   end
 end
