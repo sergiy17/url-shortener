f4eae8e27394201cdc3942640e9023f6f15b46d1
Sergiy Parkhuts <serparkhuts17@gmail.com>
2025-05-12 20:03:47 +0300

    add 'is_active' to url serializer

--- app/serializers/url_serializer.rb
+++ app/serializers/url_serializer.rb
@@ -1,7 +1,7 @@
 class UrlSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :shortened_link, :slug, :original_url, :visits, :last_visit_at
+  attributes :shortened_link, :slug, :original_url, :visits, :last_visit_at, :is_active
 
   def shortened_link
     short_url_url({ slug: Url.first.slug }.merge(Rails.application.config.action_controller.default_url_options))
@@ -13,5 +13,9 @@ class UrlSerializer < ActiveModel::Serializer
 
   def last_visit_at
     object.analytic.last_visit_at
+  end
+
+  def is_active
+    object.analytic&.last_visit_at&.>=(1.month.ago)
   end
 end
